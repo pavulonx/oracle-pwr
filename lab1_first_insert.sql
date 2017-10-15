557e463a4b4c7cfe2076921e83938687a1f7b0c9
Jakub Rozenbajger <thequetran@gmail.com>
2017-10-15 19:38:14 +0200

    solved 1 list

--- lab1_first_insert.sql
+++ lab1_first_insert.sql
@@ -1,140 +1,88 @@
-ALTER TABLE KOCURY
-  DISABLE CONSTRAINT koc_funkcja_to_fun_funkcja_fk;
-ALTER TABLE KOCURY
-  DISABLE CONSTRAINT koc_szef_to_koc_pseudo_fk;
-ALTER TABLE KOCURY
-  DISABLE CONSTRAINT koc_nr_band_to_bnd_nr_bandy_fk;
-ALTER TABLE KOCURY
-  DISABLE CONSTRAINT koc_pseudo_pk;
-ALTER TABLE WROGOWIE_KOCUROW
-  DISABLE CONSTRAINT wrgk_pseudo_to_koc_pseudo_fk;
-ALTER TABLE WROGOWIE_KOCUROW
-  DISABLE CONSTRAINT wrgk_im_wrg_to_wrg_im_wrga_fk;
+--BANDY--
+INSERT INTO Bandy(nr_bandy,nazwa,teren) VALUES(1,'SZEFOSTWO','CALOSC');
+INSERT INTO Bandy(nr_bandy,nazwa,teren) VALUES(2,'CZARNI RYCERZE','POLE');
+INSERT INTO Bandy(nr_bandy,nazwa,teren) VALUES(3,'BIALI LOWCY','SAD');
+INSERT INTO Bandy(nr_bandy,nazwa,teren) VALUES(4,'LACIACI MYSLIWI','GORKA');
+INSERT INTO Bandy(nr_bandy,nazwa,teren) VALUES(5,'ROCKERSI','ZAGRODA');
 
-INSERT ALL
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('JACEK', 'M', 'PLACEK', 'LOWCZY', 'LYSY', '2008-12-01', 67, NULL, 2)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('BARI', 'M', 'RURA', 'LAPACZ', 'LYSY', '2009-09-01', 56, NULL, 2)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('MICKA', 'D', 'LOLA', 'MILUSIA', 'TYGRYS', '2009-10-14', 25, 47, 1)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('LUCEK', 'M', 'ZERO', 'KOT', 'KURKA', '2010-03-01', 43, NULL, 3)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('SONIA', 'D', 'PUSZYSTA', 'MILUSIA', 'ZOMBI', '2010-11-18', 20, 35, 3)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('LATKA', 'D', 'UCHO', 'KOT', 'RAFA', '2011-01-01', 40, NULL, 4)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('DUDEK', 'M', 'MALY', 'KOT', 'RAFA', '2011-05-15', 40, NULL, 4)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('MRUCZEK', 'M', 'TYGRYS', 'SZEFUNIO', NULL, '2002-01-01', 103, 33, 1)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('CHYTRY', 'M', 'BOLEK', 'DZIELCZY', 'TYGRYS', '2002-05-05', 50, NULL, 1)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('KOREK', 'M', 'ZOMBI', 'BANDZIOR', 'TYGRYS', '2004-03-16', 75, 13, 3)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('BOLEK', 'M', 'LYSY', 'BANDZIOR', 'TYGRYS', '2006-08-15', 72, 21, 2)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('ZUZIA', 'D', 'SZYBKA', 'LOWCZY', 'LYSY', '2006-07-21', 65, NULL, 2)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('RUDA', 'D', 'MALA', 'MILUSIA', 'TYGRYS', '2006-09-17', 22, 42, 1)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('PUCEK', 'M', 'RAFA', 'LOWCZY', 'TYGRYS', '2006-10-15', 65, NULL, 4)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('PUNIA', 'D', 'KURKA', 'LOWCZY', 'ZOMBI', '2008-01-01', 61, NULL, 3)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('BELA', 'D', 'LASKA', 'MILUSIA', 'LYSY', '2008-02-01', 24, 28, 2)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('KSAWERY', 'M', 'MAN', 'LAPACZ', 'RAFA', '2008-07-12', 51, NULL, 4)
-INTO KOCURY (imie, plec, pseudo, funkcja, szef, w_stadku_od, przydzial_myszy, myszy_extra, nr_bandy) VALUES
-  ('MELA', 'D', 'DAMA', 'LAPACZ', 'RAFA', '2008-11-01', 51, NULL, 4)
-SELECT *
-FROM dual;
+--FUNKCJE--
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('SZEFUNIO',90,110);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('BANDZIOR',70,90);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('LOWCZY',60,70);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('LAPACZ',50,60);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('KOT',40,50);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('MILUSIA',20,30);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('DZIELCZY',45,55);
+INSERT INTO Funkcje(funkcja,min_myszy,max_myszy) VALUES('HONOROWA',6,25);
 
-INSERT ALL
-INTO BANDY (nr_bandy, nazwa, teren, szef_bandy) VALUES (1, 'SZEFOSTWO', 'CALOSC', 'TYGRYS')
-INTO BANDY (nr_bandy, nazwa, teren, szef_bandy) VALUES (2, 'CZARNI RYCERZE', 'POLE', 'LYSY')
-INTO BANDY (nr_bandy, nazwa, teren, szef_bandy) VALUES (3, 'BIALI LOWCY', 'SAD', 'ZOMBI')
-INTO BANDY (nr_bandy, nazwa, teren, szef_bandy) VALUES (4, 'LACIACI MYSLIWI', 'GORKA', 'FAFA')
-INTO BANDY (nr_bandy, nazwa, teren, szef_bandy) VALUES (5, 'ROCKERSI', 'ZAGRODA', NULL)
-SELECT *
-FROM dual;
+--WROGOWIE--
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('KAZIO',10,'CZLOWIEK','FLASZKA');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('GLUPIA ZOSKA',1,'CZLOWIEK','KORALIK');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('SWAWOLNY DYZIO',7,'CZLOWIEK','GUMA DO ZUCIA');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('BUREK',4,'PIES','KOSC');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('DZIKI BILL',10,'PIES',NULL);
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('REKSIO',2,'PIES','KOSC');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('BETHOVEN',1,'PIES','PEDIGRIPALL');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('CHYTRUSEK',5,'LIS','KURCZAK');
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('SMUKLA',1,'SOSNA',NULL);
+INSERT INTO Wrogowie(imie_wroga,stopien_wrogosci,gatunek,lapowka) VALUES ('BAZYLI',3,'KOGUT','KURA DO STADA');
 
-INSERT ALL
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('SZEFUNIO', 90, 110)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('BANDZIOR', 70, 90)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('LOWCZY', 60, 70)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('LAPACZ', 50, 60)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('KOT', 40, 50)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('MILUSIA', 20, 30)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('DZIELCZY', 45, 55)
-INTO FUNKCJE (funkcja, min_myszy, max_myszy) VALUES ('HONOROWA', 6, 25)
-SELECT *
-FROM dual;
+---KOCURY--
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('MRUCZEK','M','TYGRYS','SZEFUNIO',NULL,'2002-01-01',103,33,1);
+SELECT *FROM Kocury;
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('MICKA','D','LOLA','MILUSIA','TYGRYS','2009-10-14',25,47,1);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('CHYTRY','M','BOLEK','DZIELCZY','TYGRYS','2002-05-05',50,NULL,1);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('KOREK','M','ZOMBI','BANDZIOR','TYGRYS','2004-03-16',75,13,3);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('BOLEK','M','LYSY','BANDZIOR','TYGRYS','2006-08-15',72,21,2);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('RUDA','D','MALA','MILUSIA','TYGRYS','2006-09-17',22,42,1);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('PUCEK','M','RAFA','LOWCZY','TYGRYS','2006-10-15',65,NULL,4);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('SONIA','D','PUSZYSTA','MILUSIA','ZOMBI','2010-11-18',20,35,3);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('PUNIA','D','KURKA','LOWCZY','ZOMBI','2008-01-01',61,NULL,3);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('JACEK','M','PLACEK','LOWCZY','LYSY','2008-12-01',67,NULL,2);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('BARI','M','RURA','LAPACZ','LYSY','2009-09-01',56,NULL,2);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('ZUZIA','D','SZYBKA','LOWCZY','LYSY','2006-07-21',65,NULL,2);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('BELA','D','LASKA','MILUSIA','LYSY','2008-02-01',24,28,2);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('LATKA','D','UCHO','KOT','RAFA','2011-01-01',40,NULL,4);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('DUDEK','M','MALY','KOT','RAFA','2011-05-15',40,NULL,4);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('KSAWERY','M','MAN','LAPACZ','RAFA','2008-07-12',51,NULL,4);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('MELA','D','DAMA','LAPACZ','RAFA','2008-11-01',51,NULL,4);
+INSERT INTO Kocury(imie,plec,pseudo,funkcja,szef,w_stadku_od,przydzial_myszy,myszy_extra,nr_bandy) VALUES ('LUCEK','M','ZERO','KOT','KURKA','2010-03-01',43,NULL,3);
 
-INSERT ALL
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('KAZIO', 10, 'CZLOWIEK', 'FLASZKA')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('GLUPIA ZOSKA', 1, 'CZLOWIEK', 'KORALIK')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('SWAWOLNY DYZIO', 7, 'CZLOWIEK', 'GUMA DO ZUCIA')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('BUREK', 4, 'PIES', 'KOSC')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('DZIKI BILL', 10, 'PIES', NULL)
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('REKSIO', 2, 'PIES', 'KOSC')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('BETHOVEN', 1, 'PIES', 'PEDIGRIPALL')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('CHYTRUSEK', 5, 'LIS', 'KURCZAK')
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('SMUKLA', 1, 'SOSNA', NULL)
-INTO WROGOWIE (imie_wroga, stopien_wrogosci, gatunek, lapowka) VALUES ('BAZYLI', 3, 'KOGUT', 'KURA DO STADA')
-SELECT *
-FROM dual;
+--WROGOWIE_KOCUROW--
+select * from WROGOWIE_KOCUROW;
+delete from wrogowie_kocurow;
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('TYGRYS','KAZIO','2004-10-13','USILOWAL NABIC NA WIDLY');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('ZOMBI','SWAWOLNY DYZIO','2005-03-07','WYBIL OKO Z PROCY');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('BOLEK','KAZIO','2005-03-29','POSZCZUL BURKIEM');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('SZYBKA','GLUPIA ZOSKA','2006-09-12','UZYLA KOTA JAKO SCIERKI');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('MALA','CHYTRUSEK','2007-03-07','ZALECAL SIE');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('TYGRYS','DZIKI BILL','2007-06-12','USILOWAL POZBAWIC ZYCIA');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('BOLEK','DZIKI BILL','2007-11-10','ODGRYZL UCHO');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('LASKA','DZIKI BILL','2008-12-12','POGRYZL ZE LEDWO SIE WYLIZALA');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('LASKA','KAZIO','2009-01-07','ZLAPAL ZA OGON I ZROBIL WIATRAK');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('DAMA','KAZIO','2009-02-07','CHCIAL OBEDRZEC ZE SKORY');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('MAN','REKSIO','2009-04-14','WYJATKOWO NIEGRZECZNIE OBSZCZEKAL');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('LYSY','BETHOVEN','2009-05-11','NIE PODZIELIL SIE SWOJA KASZA');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('RURA','DZIKI BILL','2009-09-03','ODGRYZL OGON');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('PLACEK','BAZYLI','2010-07-12','DZIOBIAC UNIEMOZLIWIL PODEBRANIE KURCZAKA');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('PUSZYSTA','SMUKLA','2010-11-19','OBRZUCILA SZYSZKAMI');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('KURKA','BUREK','2010-12-14','POGONIL');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('MALY','CHYTRUSEK','2011-07-13','PODEBRAL PODEBRANE JAJKA');
+INSERT INTO Wrogowie_Kocurow(pseudo,imie_wroga,data_incydentu,opis_incydentu) VALUES ('UCHO','SWAWOLNY DYZIO','2011-07-14','OBRZUCIL KAMIENIAMI');
 
-INSERT ALL
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('TYGRYS', 'KAZIO', '2004-10-13', 'USILOWAL NABIC NA WIDLY')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('ZOMBI', 'SWAWOLNY DYZIO', '2005-03-07', 'WYBIL OKO Z PROCY')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('BOLEK', 'KAZIO', '2005-03-29', 'POSZCZUL BURKIEM')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('SZYBKA', 'GLUPIA ZOSKA', '2006-09-12', 'UZYLA KOTA JAKO SCIERKI')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('MALA', 'CHYTRUSEK', '2007-03-07', 'ZALECAL SIE')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('TYGRYS', 'DZIKI BILL', '2007-06-12', 'USILOWAL POZBAWIC ZYCIA')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('BOLEK', 'DZIKI BILL', '2007-11-10', 'ODGRYZL UCHO')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('LASKA', 'DZIKI BILL', '2008-12-12', 'POGRYZL ZE LEDWO SIE WYLIZALA')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('LASKA', 'KAZIO', '2009-01-07', 'ZLAPAL ZA OGON I ZROBIL WIATRAK')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('DAMA', 'KAZIO', '2009-02-07', 'CHCIAL OBEDRZEC ZE SKORY')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('MAN', 'REKSIO', '2009-04-14', 'WYJATKOWO NIEGRZECZNIE OBSZCZEKAL')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('LYSY', 'BETHOVEN', '2009-05-11', 'NIE PODZIELIL SIE SWOJA KASZA')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('RURA', 'DZIKI BILL', '2009-09-03', 'ODGRYZL OGON')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('PLACEK', 'BAZYLI', '2010-07-12', 'DZIOBIAC UNIEMOZLIWIL PODEBRANIE KURCZAKA')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('PUSZYSTA', 'SMUKLA', '2010-11-19', 'OBRZUCILA SZYSZKAMI')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('KURKA', 'BUREK', '2010-12-14', 'POGONIL')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('MALY', 'CHYTRUSEK', '2011-07-13', 'PODEBRAL PODEBRANE JAJKA')
-INTO Wrogowie_Kocurow (pseudo, imie_wroga, data_incydentu, opis_incydentu) VALUES
-  ('UCHO', 'SWAWOLNY DYZIO', '2011-07-14', 'OBRZUCIL KAMIENIAMI')
-SELECT *
-FROM dual;
 
-ALTER TABLE KOCURY
-  ENABLE CONSTRAINT koc_funkcja_to_fun_funkcja_fk;
-ALTER TABLE KOCURY
-  ENABLE CONSTRAINT koc_szef_to_koc_pseudo_fk;
-ALTER TABLE KOCURY
-  ENABLE CONSTRAINT koc_nr_band_to_bnd_nr_bandy_fk;
-ALTER TABLE KOCURY
-  ENABLE CONSTRAINT koc_pseudo_pk;
-ALTER TABLE WROGOWIE_KOCUROW
-  ENABLE CONSTRAINT wrgk_pseudo_to_koc_pseudo_fk;
-ALTER TABLE WROGOWIE_KOCUROW
-  ENABLE CONSTRAINT wrgk_im_wrg_to_wrg_im_wrga_fk;
+UPDATE Bandy
+SET SZEF_BANDY='TYGRYS'
+WHERE NR_BANDY =1;
+
+UPDATE Bandy
+SET SZEF_BANDY='LYSY'
+WHERE NR_BANDY =2;
+
+UPDATE Bandy
+SET SZEF_BANDY='ZOMBI'
+WHERE NR_BANDY =3;
+
+UPDATE Bandy
+SET SZEF_BANDY='RAFA'
+WHERE NR_BANDY =4;
---
+++ lab1zadania.sql
@@ -0,0 +1,173 @@
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+--1--
+SELECT
+  IMIE_WROGA     "WROG",
+  OPIS_INCYDENTU "PRZEWINA"
+FROM WROGOWIE_KOCUROW
+WHERE extract(YEAR FROM DATA_INCYDENTU) = 2009;
+
+--2--
+SELECT
+  IMIE,
+  FUNKCJA,
+  W_STADKU_OD "Z NAMI OD"
+FROM KOCURY
+WHERE PLEC = 'D' AND W_STADKU_OD BETWEEN '2005-09-01' AND '2007-07-31';
+
+--3--
+SELECT
+  imie_wroga       "WROG",
+  gatunek,
+  stopien_wrogosci "STOPIEN WROGOSCI"
+FROM Wrogowie
+WHERE lapowka IS NULL
+ORDER BY stopien_wrogosci ASC;
+
+--4--
+SELECT imie || ' zwany ' || pseudo || ' (fun. ' || funkcja || ') lowi myszki w bandzie'
+       || nr_bandy || ' od ' || w_stadku_od "WSZYSTKO O KOCURACH"
+FROM Kocury
+WHERE plec = 'M'
+ORDER BY w_stadku_od DESC, pseudo ASC;
+
+--5--
+SELECT
+  pseudo,
+  REGEXP_REPLACE(
+      REGEXP_REPLACE(pseudo, 'L', '%', 1, 1),
+      'A', '#', 1, 1
+  ) "Po wymianie A na # oraz L na %"
+FROM Kocury
+WHERE pseudo LIKE '%L%' AND pseudo LIKE '%A%';
+
+--6 nie dziala add months--
+SELECT
+  imie,
+  w_stadku_od                               "w stadku",
+  ROUND(NVL((przydzial_myszy * 0.9), 0), 0) "Zjadal",
+  ADD_MONTHS(w_stadku_od, 6)                "Podwyzka",
+  przydzial_myszy                           "Zjada"
+FROM Kocury
+WHERE
+  (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM w_stadku_od)) > 7 AND EXTRACT(MONTH FROM w_stadku_od) BETWEEN 3 AND 9;
+
+--7--
+SELECT *
+FROM kocury;
+SELECT
+  imie,
+  NVL(przydzial_myszy, 0) * 3 "MYSZY KWARTALNE",
+  NVL(myszy_extra, 0) * 3     "KWARTALNE DODATKI"
+FROM Kocury
+WHERE (przydzial_myszy) > NVL(myszy_extra, 0) * 2 AND NVL(przydzial_myszy, 0) >= 55;
+
+--8--
+SELECT
+  imie,
+  CASE
+  WHEN (NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12 = 660
+    THEN 'LIMIT'
+  WHEN (NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12 > 660
+    THEN
+      TO_CHAR((NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12)
+  ELSE 'Ponizej 660' END "Zjada rocznie"
+FROM Kocury
+ORDER BY imie ASC;
+
+--9
+SELECT
+  PSEUDO,
+  W_STADKU_OD "W STADKU",
+  CASE
+  WHEN EXTRACT(DAY FROM W_STADKU_OD) BETWEEN 1 AND 15
+    THEN
+      CASE
+      WHEN NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'WEDNESDAY') < '2017-10-26'
+        THEN NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'WEDNESDAY')
+      ELSE
+        NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'WEDNESDAY')
+      END
+  ELSE
+    NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'WEDNESDAY')
+  END         "WYPLATA"
+FROM Kocury;
+
+--10--
+SELECT CASE COUNT(pseudo)
+       WHEN 1
+         THEN pseudo || '-Unikalny'
+       ELSE pseudo || '-Nieunikalny'
+       END "Unikalnosc atr.PSEUDO"
+FROM Kocury
+GROUP BY pseudo;
+
+--szef--
+
+SELECT CASE COUNT(szef)
+       WHEN 1
+         THEN szef || '-Unikalny'
+       ELSE szef || '-Nieunikalny'
+       END "Unikalnosc atr.SZEF"
+FROM Kocury
+WHERE szef IS NOT NULL
+GROUP BY szef;
+
+--11--
+SELECT
+  pseudo,
+  COUNT(imie_wroga) "Liczba wrogow"
+FROM WROGOWIE_KOCUROW
+GROUP BY pseudo
+HAVING COUNT(imie_wroga) >= 2;
+
+--12--
+
+SELECT
+  'Liczba kotow='                                    "  ",
+  COUNT(*)                                           " ",
+  'lowi jako'                                        "   ",
+  funkcja                                            "     ",
+  'i zjada max.'                                     "      ",
+  MAX(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) "       "
+FROM Kocury
+WHERE
+  funkcja <> 'SZEFUNIO'
+  AND plec <> 'M'
+GROUP BY funkcja
+HAVING AVG(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) > 50;
+
+--13--
+SELECT
+  nr_bandy                     "Nr Bandy",
+  plec                         "Plec",
+  MIN(NVL(przydzial_myszy, 0)) "Minimalny przydzial"
+FROM Kocury
+GROUP BY nr_bandy, plec;
+
+--14--
+SELECT
+  LEVEL    "Poziom",
+  pseudo   "Pseudonim",
+  funkcja  "Funkcja",
+  nr_bandy "Nr Bandy"
+FROM Kocury
+WHERE plec = 'M'
+CONNECT BY PRIOR pseudo = szef
+START WITH funkcja = 'BANDZIOR';
+
+--15--
+SELECT
+  RPAD((LPAD((LEVEL - 1), (LEVEL - 1) * 4 + 1, '===>')), 16) ||
+  LPAD(' ', (LEVEL - 1) * 4) || imie "Hierarchia",
+  NVL(szef, 'Sam sobie panem')       "Pseudo szefa",
+  funkcja                            "Funkcja"
+FROM Kocury
+WHERE NVL(myszy_extra, 0) > 0
+CONNECT BY PRIOR pseudo = szef
+START WITH szef IS NULL;
+
+--16--
+SELECT LPAD(' ', 3 * (LEVEL - 1)) || pseudo "Droga sluzbowa"
+FROM Kocury
+CONNECT BY pseudo = PRIOR szef
+START WITH plec = 'M' AND (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM w_stadku_od)) > 7 AND NVL(myszy_extra, 0) = 0;
